686d45d5959ee9f06e0770c28151622db3be0e21
Khalifa I9 <k.rmili@exchange-data.com>
2021-05-22 15:33:08 +0100

    single quote error -> warn

--- .eslintrc.js
+++ .eslintrc.js
@@ -56,7 +56,7 @@ module.exports = {
       { allowAllPropertiesOnSameLine: true }
     ],
     'quote-props': ['error', 'as-needed'],
-    quotes: ['error', 'single'],
+    quotes: ['warn', 'single'],
     semi: ['error', 'never'],
     'semi-spacing': 'error',
     'space-before-blocks': 'error',
--- config-overrides.js
+++ config-overrides.js
@@ -4,7 +4,7 @@ const rewireAliases = require('react-app-rewire-aliases')
 
 module.exports = function override(config, env) {
   require('react-app-rewire-postcss')(config, {
-    plugins: loader => [require('postcss-rtl')()]
+    plugins: (loader) => [require('postcss-rtl')()]
   })
 
   config = rewireAliases.aliasesOptions({
